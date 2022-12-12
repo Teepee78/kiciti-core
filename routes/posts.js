795441cedf12d53ff282d7c28e3825f219cf74f5
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-12-12 16:51:46 +0100

    Create GET endpoint to get all posts for a user

--- routes/posts.js
+++ routes/posts.js
@@ -29,7 +29,7 @@ const router = Router();
  *       "200":
  *         description: OK
  *       "403":
- *         description: Bad Request
+ *         description: Forbidden
  */
 router.post("/", authenticate, async (req, res) => {
   try {
@@ -45,11 +45,56 @@ router.post("/", authenticate, async (req, res) => {
 
     // Add post id to user posts
     user.posts.push(post._id);
+    await user.save();
 
     res.json({
       message: "Post created successfully!",
       status: "OK",
       post: _.omit(post.toObject(), [ "__v"])
+    });
+  }
+  catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+
+/**
+ * @openapi
+ *
+ * /api/posts/{user_id}:
+ *   get:
+ *     summary: Get all posts for a user
+ *     tags: [Posts]
+ *     parameters:
+ *      - in: path
+ *        name: user_id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: id of user (string)
+ *     responses:
+ *       "200":
+ *         description: OK
+ *       "403":
+ *         description: Forbidden
+ */
+router.get('/:user_id', authenticate, async (req, res) => {
+  try {
+    // Get user by id
+    let user = await User.findById(req.params.user_id);
+
+    // Get all posts by user
+    let posts = [];
+    for (let post_id of user.posts) {
+      let post = await Post.findById(post_id);
+      posts.push(post);
+    }
+
+    res.json({
+      status: "OK",
+      posts: posts
     });
   }
   catch (error) {
