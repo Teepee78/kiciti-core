fe3a817a23344188f1511ca341dbb9d36591840e
Teepee78 <eniolatoluwalase78@gmail.com>
2023-01-05 11:29:07 +0100

    Authentication fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,11 +6,13 @@ async function authenticate (req, res, next) {
   console.log("\nAuthenticating User...");
   // check for jwt
   if (req.cookies["X-auth-token"] === undefined && req.headers['X-auth-token'] === undefined) {
+    console.log("Here 1");
     return res.status(400).json({message: "User not logged in"});
   }
   // verify user is signed in
   let user = jwt.verify(req.cookies["X-auth-token"], process.env.KICITI_JWT);
   if (!user) {
+    console.log("Checking headers");
     user = jwt.verify(req.headers["X-Auth-Token"], process.env.KICITI_JWT);
     if (!user) return res.status(400).json({message: "User not logged in"});
   }
